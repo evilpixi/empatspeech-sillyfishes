d60fcf3649fe0b84a6eaa313edd64d725c11888b
evilpixi <tatosalvi@gmail.com>
2024-12-26 22:59:06 -0300

    updated build

--- esbuild/build.prod.mjs
+++ esbuild/build.prod.mjs
@@ -5,8 +5,10 @@ import inlineImage from 'esbuild-plugin-inline-image';
 
 let msgPhaser = {
     name: 'msg-phaser',
-    setup(build) {
-        build.onEnd(() => {
+    setup (build)
+    {
+        build.onEnd(() =>
+        {
             const line = "---------------------------------------------------------";
             const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
             process.stdout.write(`${line}\n${msg}\n${line}\n`);
@@ -16,13 +18,15 @@ let msgPhaser = {
     },
 }
 
-const builder = async () => {
+const builder = async () =>
+{
     await build({
         entryPoints: ['./src/main.ts'],
+        tsconfig: './tsconfig.json',
         bundle: true,
         minify: true,
         sourcemap: false,
-        target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+        target: ['es2020', 'chrome80', 'firefox70', 'safari13', 'edge18'],
         outfile: './dist/bundle.min.js',
         plugins: [
             clean({
